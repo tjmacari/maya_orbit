bbfce4ed563396db74dc151bb86d3a0890ae97f4
Thomas Macari <tjmacari@gmail.com>
2017-02-13 03:33:48 -0800

    Clean up python scripts

--- scripts/randomInstances.py
+++ scripts/randomInstances.py
@@ -1,18 +1,55 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
-{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+# randomInstances.py
 
-\f0\fs24 \cf0 # aimAtFirst.py\
-\
-import maya.cmds as cmds\
-\
-selectionList = cmds.ls( orderedSelection=True )\
-\
-if len( selectionList ) >= 2:\
-    targetName = selectionList[0]\
-    selectionList.remove( targetName )\
-    for objectName in selectionList:\
-        cmds.aimConstraint( targetName, objectName, aimVector=[0,1,0] )\
-}
+import maya.cmds as cmds
+import random
+
+random.seed ( 1234 )
+
+result = cmds.ls( orderedSelection=True ) # create list in order of selection
+
+# str - convert contents to plain text
+# ie. result: [u'myCube2', u'playCube2'] - the transform and the orig cube - u: unicode
+#print 'result: ' + str(result)
+print 'result: %s' % ( result ) #same as above
+
+# DAG - Directed Ayclic Graph (view in Window -> Outliner to see cube object and shape node)
+# Duplicate cube by selecting in Outliner, then Edit -> Dup special (the box) then select instance/apply
+
+transformName = result[0] # ie. result = ['myCube1', 'polyCube1'], so grab 'myCube1'
+
+# group cubes neatly together
+instanceGroupName = cmds.group( empty=True, name=transformName + '_instance_grp#' )
+
+for i in range ( 0, 50 ):
+    
+    instanceResult = cmds.instance( transformName, name=transformName + '_instance#' )
+    
+    # add each instance to our group above
+    cmds.parent( instanceResult, instanceGroupName )
+
+    #print 'instanceResult: ' + str( instanceResult)
+    
+    x = random.uniform( -10, 10 )
+    y = random.uniform( 0, 20 )
+    z = random.uniform( -10, 10 )
+
+    cmds.move (x, y, z, instanceResult ) # move over 10 units
+    
+    xRot = random.uniform( 0, 360 )
+    yRot = random.uniform( 0, 360 )
+    zRot = random.uniform( 0, 360 )
+    
+    cmds.rotate( xRot, yRot, zRot, instanceResult )
+    
+    scalingFactor = random.uniform( 0.3, 1.5 )
+    
+    cmds.scale( scalingFactor, scalingFactor, scalingFactor, instanceResult )
+    
+cmds.hide( transformName ) # hide orig cube
+    
+cmds.xform( instanceGroupName, centerPivots=True ) # Center the group pivot
+
+# to make sure all items are facing the center:
+# click Windows / selection/preferences / Selection text item, check 'Track Selection Order'
+
+# can create any type of object (sphere, pyramid, etc.) and run this script, still works!
